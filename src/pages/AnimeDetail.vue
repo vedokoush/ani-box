0ba6233988ee357531b24a9dd29d13639e2ee47c
skotish06 <thaitrung455@gmail.com>
2025-09-10 18:00:36 +0700

    restore

--- src/pages/AnimeDetail.vue
+++ src/pages/AnimeDetail.vue
@@ -116,7 +116,24 @@ onMounted(async () => {
     <div class="anime-episode">
 
     </div>
-    <Recommendation />
+<!--    <Recommendation />-->
+    <div class="more-like-this" v-if="recommendations.length">
+      <h2>More like this</h2>
+      <div class="recommend-list">
+        <div
+          v-for="rec in recommendations"
+          :key="rec.entry.mal_id"
+          class="recommend-item"
+        >
+          <img
+            :src="rec.entry.images.jpg.large_image_url"
+            :alt="rec.entry.title"
+            class="recommend-img"
+          />
+          <p class="recommend-title">{{ rec.entry.title }}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -251,6 +268,30 @@ onMounted(async () => {
   font-size: 1.5rem;
   margin-bottom: 20px;
   color: #fff;
+}
+
+.recommend-list {
+  display: flex;
+  gap: 20px;
+  flex-wrap: wrap;
+}
+
+.recommend-item {
+  width: 160px;
+  text-align: center;
+  color: #ccc;
+}
+
+.recommend-img {
+  width: 100%;
+  border-radius: 8px;
+  object-fit: cover;
+  margin-bottom: 10px;
+}
+
+.recommend-title {
+  font-size: 0.9rem;
+  line-height: 1.3;
 }
 
 .cast-list::-webkit-scrollbar {
